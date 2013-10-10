485ad5239630b4fb786cbcb6ad68c9a3a4d61b24
Erin <audrey2otoo@gmail.com>
2013-10-09 18:40:28 -0700

    its better!

--- RPSLS.js
+++ RPSLS.js
@@ -47,14 +47,31 @@ prompt.get('weapon', function(err, result) {
 
 
 var compare = function (choice1, choice2) {
+
+if (choice2 === 1) {
+    console.log("The computer chose: Rock");
+}
+else if (choice2 === 2) {
+    console.log("The computer chose: Paper");
+}
+else if (choice2 === 3) {
+    console.log("The computer chose: Scissors");
+}
+else if (choice2 === 4) {
+    console.log("The computer chose: Lizard");
+}
+else {
+    console.log("The computer chose: Spock");
+}
+
 //It's a tie
 if (choice1 === choice2) {
-      console.log("You've thwarted me this time young padawan!");
+      console.log("It's a tie! ... You've thwarted me this time young padawan!");
 }
 //Rock
-if (choice1 === "rock" || choice1 === 1) {
+else if (choice1 === "rock" || choice1 === 1) {
     if (choice2 === 2) {
-        console.log("Paper covers rock! Weak choice, but rules are rules.");
+        console.log("Paper covers rock! You lose!");
     }
     else if (choice2 === 3) {
         console.log("Rock crushes scissors!");
@@ -63,72 +80,73 @@ if (choice1 === "rock" || choice1 === 1) {
         console.log("Rock crushes lizard! You just crushed a lizard, mad man!");
     }
     else { //choice2 === 5
-        console.log("Spock vaporizes rock! Spock is awesome!");
+        console.log("Spock vaporizes rock! You lose!");
     }
 }
 //Paper
 else if (choice1 === "paper" || choice1 === 2) {
     if (choice2 === 1) {
-        console.log("Paper covers rock. I know, it's a weird rule!");
+        console.log("Paper covers rock. Weak choice, but rules are rules, you win!");
     }
     else if (choice2 === 3) {
-        console.log("Scissors cut paper!");
+        console.log("Scissors cut paper! You lose!");
     }
     else if (choice2 === 4) {
-        console.log("Lizard eats paper! Hope it digests well!");
+        console.log("Lizard eats paper! Hope it digests well! You lose!");
     }
     else { //choice2 === 5
-        console.log("Paper disproves Spock! Shocking, right?");
+        console.log("Paper disproves Spock! Shocking, but it still happened, you win!");
     }
 }
 //Scissors
 else if (choice1 === "scissors" || choice1 === 3) {
     if (choice2 === 1) {
-        console.log("Rock crushes scissors!");
+        console.log("Rock crushes scissors! You lose!");
     }
     else if (choice2 === 2){
-        console.log("Scissors cut paper!");
+        console.log("Scissors cut paper! You win!");
     }
     else if (choice2 === 4) {
-        console.log("Scissors decapitates lizard!");
+        console.log("Scissors decapitates lizard! Poor lizard...You win!");
     }
     else { //choice2 === 5
-        console.log("Spock smashes scissors! Spock smash!");
+        console.log("Spock smashes scissors! Spock smash! You lose!");
     }
 }
 //Lizard
 else if (choice1 === "lizard" || choice1 === 4) {
     if (choice2 === 1) {
-        console.log("Rock crushes lizard! You just crushed a lizard, mad man!");
+        console.log("Rock crushes lizard! Poor lizard! You lose!");
     }
     else if (choice2 === 2) {
-        console.log("Lizard eats paper! Hope it digests well!");
+        console.log("Lizard eats paper! Terrible diet. You win!");
     }
     else if (choice2 === 3) {
-        console.log("Scissors decapitates lizard");
+        console.log("Scissors decapitates lizard! So sad. You lose!");
     }
     else { //choice2 === 5
-        console.log("Lizard poisons Spock!");
+        console.log("Lizard poisons Spock! Why would you do that!? You win.");
     }
 }
 //Spock
 else if (choice1 === "spock" || choice1 === 5) {
     if (choice2 === 1) {
-        console.log("Spock vaporizes rock! Spock is awesome!");
+        console.log("Spock vaporizes rock! Spock is awesome! You win!");
   }
     else if (choice2 === 2) {
-        console.log("Paper disproves Spock! Shocking, right?");
+        console.log("Paper disproves Spock! Spock was wrong? You lose!");
   }
     else if (choice2 === 3) {
-        console.log("Spock smashes scissors! Spock smash!");
+        console.log("Spock smashes scissors! Spock smash! You win!");
   }
     else if (choice2 === 4) {
-        console.log("Lizard poisons Spock");
+        console.log("Lizard poisons Spock. Spock's only weakness apparently. You lose.");
     }
 }
 else { //Something didn't work
     console.log("Something isn't right, sir.");
 }}
+
 /*
 //debuggging::randomizes user input function "weapon" to test compare function
 weapon = Math.floor((Math.random()*5)+1) //integer between 1 and 5
